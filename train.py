9d04a7156d31a8f555c2185aa8f5c0b26d5745be
Vincent La <vincentla20877@gmail.com>
2024-04-28 15:33:00 -0400

    10000 and 50000 step runs

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from torch.utils.tensorboard import SummaryWriter
 # from apex import amp
 # from apex.parallel import DistributedDataParallel as DDP
 
-#from models.modeling import VisionTransformer, CONFIGS
+# from models.modeling import VisionTransformer, CONFIGS
 from models.modeling_bitlinear import VisionTransformer, CONFIGS
 from utils.scheduler import WarmupLinearSchedule, WarmupCosineSchedule
 from utils.data_utils import get_loader
